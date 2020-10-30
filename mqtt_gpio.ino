3aa230cadab0c8e39aab499d780c8ad1974af772
Yannik <yannik@yannik-sc.de>
2020-10-30 20:49:20 +0100

    Add input, Change control to mode and state for all

--- mqtt_gpio.ino
+++ mqtt_gpio.ino
@@ -9,61 +9,160 @@
 #include <Vector.h>
 #include "settings.h" // This file is meant to contain settings like WIFI settings
 
-EspMQTTClient client(
-  WIFI_SSID,
-  WIFI_PASSWORD,
-  MQTT_BROKER_HOST,
-  MQTT_BROKER_USERNAME,
-  MQTT_BROKER_PASSWORD,
-  DEVICE_NAME,
-  MQTT_BROKER_PORT
-);
+enum PinMode
+{
+  UNSET = 0,
+  DIGITAL_OUT = 1,
+  PWM_OUT = 2,
+  DIGITAL_IN = 3
+};
 
-void setup() {
+PinMode modes[100];
+int states[100];
+
+EspMQTTClient client(
+    WIFI_SSID,
+    WIFI_PASSWORD,
+    MQTT_BROKER_HOST,
+    MQTT_BROKER_USERNAME,
+    MQTT_BROKER_PASSWORD,
+    DEVICE_NAME,
+    MQTT_BROKER_PORT);
+
+void setup()
+{
   Serial.begin(115200);
   Serial.print("Connecting as ");
   Serial.println(DEVICE_NAME);
 
-# ifdef DEBUG
+#ifdef DEBUG
   client.enableDebuggingMessages();
-# endif
-}
-
-void handleGpio(const String &payload) {
-  DynamicJsonDocument doc(512);
-  deserializeJson(doc, payload);
-
-  uint8_t pin = doc["pin"];
-  uint8_t value = doc["value"];
-
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, value);
-}
-
-#ifndef ESP32 // As the ESP32 has not implemented the analogWrite yet
-void handlePwm(const String &payload) {
-  DynamicJsonDocument doc(512);
-  deserializeJson(doc, payload);
-
-
-  uint8_t pin = doc["pin"];
-  uint16_t value = doc["value"];
-
-  pinMode(pin, OUTPUT);
-  analogWrite(pin, value);
-}
 #endif
+}
 
-void onConnectionEstablished() {
+void handleGpio(const String &topicStr, const String &payload)
+{
+  String pinStr = String(topicStr);
+
+  pinStr.replace(DEVICE_NAME "/", "");
+  pinStr.replace("/state", "");
+  int pin = pinStr.toInt();
+  int state = payload.toInt();
+
+  if (pin > 99 || modes[pin] == PinMode::DIGITAL_IN)
+  {
+    return;
+  }
+
+  states[pin] = state;
+}
+
+void handleSetMode(const String &topicStr, const String &payload)
+{
+  String pinStr = String(topicStr);
+
+  pinStr.replace(DEVICE_NAME "/", "");
+  pinStr.replace("/state", "");
+  int pin = pinStr.toInt();
+
+  if (pin > 99)
+  {
+    return;
+  }
+
+  modes[pin] = translateStringToEnum(payload);
+}
+
+PinMode translateStringToEnum(const String &name)
+{
+  if (name == "DIGITAL_OUT")
+    return PinMode::DIGITAL_OUT;
+
+  if (name == "PWM_OUT")
+    return PinMode::PWM_OUT;
+
+  if (name == "DIGITAL_IN")
+    return PinMode::DIGITAL_IN;
+
+  return PinMode::UNSET;
+}
+
+void onConnectionEstablished()
+{
   Serial.println("Ready");
 
-  client.publish(DEVICE_NAME "/application", "mqtt_gpio", true);
-  client.subscribe(DEVICE_NAME "/gpio", handleGpio);
-# ifndef ESP32 // As the ESP32 has not implemented the analogWrite yet
-  client.subscribe(DEVICE_NAME "/pwm", handlePwm);
-# endif
+  client.publish(DEVICE_NAME "/__application", "mqtt_gpio", true);
+  client.subscribe(DEVICE_NAME "/+/state", handleGpio);
+  client.subscribe(DEVICE_NAME "/+/mode", handleSetMode);
 }
 
-void loop() {
+void handlePins()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    if (modes[i] == DIGITAL_OUT)
+    {
+      handleDigitalOutput(i);
+
+      continue;
+    }
+
+    if (modes[i] == PWM_OUT)
+    {
+      handlePwmOutput(i);
+
+      continue;
+    }
+
+    if (modes[i] == DIGITAL_IN)
+    {
+      handleDigitalInput(i);
+
+      continue;
+    }
+  }
+}
+
+void handleDigitalOutput(int pin)
+{
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, states[pin] > 0 ? 1 : 0);
+}
+
+void handlePwmOutput(int pin)
+{
+  pinMode(pin, OUTPUT);
+  analogWrite(pin, states[pin]);
+}
+
+void handleDigitalInput(int pin)
+{
+  pinMode(pin, INPUT);
+  int lastState = states[pin] > 0 ? 1 : 0;
+  states[pin] = digitalRead(pin);
+
+  if (lastState == states[pin])
+  {
+    return;
+  }
+
+  String publishBase = String(DEVICE_NAME "/");
+
+  client.publish(publishBase + pin + "/toggle", "", false);
+  client.publish(publishBase + pin + "/state", String(states[pin]), true);
+
+  if (states[pin] == 0)
+  {
+    client.publish(publishBase + pin + "/toggle/off", "", false);
+
+    return;
+  }
+
+  client.publish(publishBase + pin + "/toggle/on", "", false);
+}
+
+void loop()
+{
   client.loop();
+  handlePins();
 }
